c646a487eaff7cf861642392ee2ff16b2eead1b0
Trisotto <longaraitrisotto.flt@gmail.com>
2021-04-05 21:34:38 -0300

    Atualização do Algoritmo de Reconhecimento de Expressão

--- pytomato/gramatica.py
+++ pytomato/gramatica.py
@@ -1,3 +1,7 @@
+from pytomato.conversion_af_gr import (
+    search_initial,
+    search_non_terminal,
+    search_terminal)
 """Conversão de Texto x Objeto
 Recebe duas strings, Gramática e Nome, do usuário e as
 converte em objeto e insere na estrutura de dicionário.
@@ -10,7 +14,7 @@ def texto_para_obj(text, nome):
         if '->' in linha:
             nomeEstado, estado = linha.split('->')
             transicoes = estado.split('|')
-            
+
             gramatica[nomeEstado] = transicoes
     
     return {
@@ -34,6 +38,56 @@ def obj_para_texto(objeto):
     
     return texto
 
+"""Reconhecimento de Expressão
+
+Recebe uma string e um dicionario, expressão e gramática,
+do usuário e retorna se a expressão é válida ou não.
+"S -> aS | aA \n A -> bA | b"
+-> aab
+"""
+def isValidExpression(expression, grammar, previousCharacter = None):
+    terminalSymbolsList = search_terminal(grammar)
+    nonTerminalSymbolsList = search_non_terminal(grammar)
+    initialSymbol = search_initial(grammar)
+    
+    if (len(expression) == 0 and '&' in grammar[initialSymbol]):
+        return True
+
+    character = expression[0]
+
+    if (len(expression) == 1):
+        possibleTerminalSymbolsList = [v for l in grammar.values() for v in l]
+        for possibleTerminalSymbol in possibleTerminalSymbolsList:
+            if ((not previousCharacter) and len(possibleTerminalSymbol) == 1):
+                if (character != possibleTerminalSymbol):
+                    continue
+
+                return True
+
+            if (not previousCharacter):
+                return False
+
+            if (possibleTerminalSymbol != previousCharacter):
+                continue
+
+            targetCharacter = possibleTerminalSymbol[-1]
+            
+            if (targetCharacter in terminalSymbolsList):
+                return character == targetCharacter
+
+            terminalSymbols = [v[1] for v in grammar[targetCharacter] if len(v) == 2]
+            for terminalSymbol in terminalSymbols:
+                if (terminalSymbol == character):
+                    return True
+
+        return False
+
+    for c in expression:
+        if not isValidExpression(c, grammar, previousCharacter):
+            return False
+        previousCharacter = c
+    return True
+
 """Teste
 Main criado para testar as funções.
 """
@@ -44,3 +98,5 @@ if __name__ == '__main__':
             C -> aB | bA | cC | c"
 
     print(texto_para_obj(texto, 'nome'))
+
+    print(isValidExpression("a",  { "S": ["aA", "bB", "cS"], "A": ["aS", "bC", "b", "cA"], "B": ["aC", "a", "bS", "cB"], "C": ["aB", "bA", "cC", "c"] }, "a"))
